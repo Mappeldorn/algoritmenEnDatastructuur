4d017fd030138b6210c4fd60403b8707ed1d5ae1
JesseOost <89914221+JesseOost@users.noreply.github.com>
2023-04-06 15:14:43 +0200

    transalted comments

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -5,9 +5,9 @@ public class BubbleSort<T extends Comparable<T>> {
         for (int i = 0; i < length - 1; i++) {
             for (int j = 0; j < length - i - 1; j++) {
 
-                // Vergelijken of het volgende element groter is dan de huidige
+                // Compare if the next element is greater than the current
                 if (arr[j].compareTo(arr[j + 1]) > 0) {
-                    // Omdraaien
+                    // Swap
                     T temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
