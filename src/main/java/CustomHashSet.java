0472a380f9d8b9832ac2d0c2013b29c7c27d761d
JesseOost <89914221+JesseOost@users.noreply.github.com>
2023-04-06 15:12:43 +0200

    added comments to code

--- src/main/java/CustomHashSet.java
+++ src/main/java/CustomHashSet.java
@@ -7,16 +7,23 @@ public class CustomHashSet<T> {
         size = 0;
     }
 
+    /**
+     * Add an element to the set
+     * @param element The element to add
+     * @return True if the element was added, false if the element was already in the set
+     */
     public boolean add(T element) {
         if (contains(element)) {
             return false;
         }
+        // Resize if the table is full
         if (size >= table.length) {
             resize();
         }
 
         int index = hash(element);
 
+        // If index is already taken, find the next free index
         while (table[index] != null) {
             index = (index + 1) % table.length;
         }
@@ -26,9 +33,16 @@ public class CustomHashSet<T> {
         return true;
     }
 
+    /**
+     * Remove an element from the set
+     * @param element The element to remove
+     * @return True if the element was removed, false if the element was not in the set
+     */
     public boolean remove(T element) {
         int index = hash(element);
+
         while (table[index] != null) {
+            // If the element is found, remove it
             if (table[index].equals(element)) {
                 table[index] = null;
                 size--;
@@ -39,9 +53,16 @@ public class CustomHashSet<T> {
         return false;
     }
 
+    /**
+     * Check if the set contains an element
+     * @param element The element to check
+     * @return True if the element is in the set, false if the element is not in the set
+     */
     public boolean contains(T element) {
         int index = hash(element);
+
         while (table[index] != null) {
+            // If the element is found, return true
             if (table[index].equals(element)) {
                 return true;
             }
@@ -50,14 +71,23 @@ public class CustomHashSet<T> {
         return false;
     }
 
+    /**
+     * Get the size of the set
+     * @return The size of the set
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Resize the set
+     */
     private void resize() {
         Object[] oldTable = table;
         table = new Object[oldTable.length * 2];
         size = 0;
+
+        // Rehash all elements
         for (Object element : oldTable) {
             if (element != null) {
                 add((T) element);
@@ -65,6 +95,11 @@ public class CustomHashSet<T> {
         }
     }
 
+    /**
+     * Hash an element
+     * @param element The element to hash
+     * @return The hash of the element
+     */
     private int hash(T element) {
         return Math.abs(element.hashCode() % table.length);
     }
